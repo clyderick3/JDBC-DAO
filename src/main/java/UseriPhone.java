f474573547b67b2e962f3f2b45ff36fd7556bb45
Clyde Broderick <clyderick3@gmail.com>
2022-04-19 18:16:54 -0400

    Generated getters and setters for iPhone.

--- src/main/java/UseriPhone.java
+++ src/main/java/UseriPhone.java
@@ -18,5 +18,43 @@ public class UseriPhone {
         this.faceID = faceID;
     }
 
+    public String getManufacturer() {
+        return manufacturer;
+    }
 
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public boolean isFaceID() {
+        return faceID;
+    }
+
+    public void setFaceID(boolean faceID) {
+        this.faceID = faceID;
+    }
 }
