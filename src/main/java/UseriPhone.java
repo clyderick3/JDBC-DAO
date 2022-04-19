60bc59a7af6ce83990860bc82d1590443a46f38d
Clyde Broderick <clyderick3@gmail.com>
2022-04-19 18:07:18 -0400

    Added instance variables and started creating constructor.

---
+++ src/main/java/UseriPhone.java
@@ -0,0 +1,16 @@
+public class UseriPhone {
+
+    private String manufacturer;
+    private String size;
+    private String color;
+    private String model;
+    private boolean faceID;
+
+    public UseriPhone(){
+
+    }
+
+    public UseriPhone(String manufacturer, String size, String color, String model, boolean faceID){
+
+    }
+}
